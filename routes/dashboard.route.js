ba2400b8ddcc58e8bd2164e63ff7f14bb46830d4
Amine <aminehouidi90@gmail.com>
2022-01-18 19:12:34 +0100

    dashboard routes

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -6,20 +6,34 @@ const UserModel = require("../models/User.model");
 //Dashboard READ
 router.get("/:userId", function (req, res) {
     const userId = req.params.userId
-    console.log(userId, "USER ID HERE")
+    // console.log(userId, "USER ID HERE")
     UserModel.findById(userId).populate("portfolios") // PortfolioModel.find({_id: userId})
         .then((user) => {
             res.render("dashboard/dashboardUser", { user })
-            console.log(user, "USER HERE")
+            // console.log(user, "USER HERE")
         })
         .catch((err) => console.log(err))
-})
+});
 
 //Dashboard CREATE Portfolio
-router.post("/:userId", function (req, res, next) {
+router.post("/:userId/create", function (req, res, next) {
     PortfolioModel.create(req.body)
         .then((newPortfolio) => res.json(newPortfolio))
-        .catch(next)
+        .catch((err) => next(err))
+});
+
+//Dashboard UPDATE Portfolio
+router.patch("/:userId/update/:id", function (req, res, next) {
+    PortfolioModel.findByIdAndUpdate(req.params.id, req.body)
+        .then((editPortfolio) => res.json(editPortfolio))
+        .catch((err) => next(err))
+});
+
+//Dashboard DELETE Portfolio
+router.delete("/:userId/delete/:id", function (req, res, next) {
+    PortfolioModel.findByIdAndDelete(req.params.id)
+        .then((deletePortfolio) => res.json(deletePortfolio))
+        .catch((err) => next(err))
 });
 
 module.exports = router;
