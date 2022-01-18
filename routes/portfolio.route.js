71b45126f472da4ea605c9be0e15069093e38797
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-01-18 15:04:23 +0100

    Changes Arthur

--- routes/portfolio.route.js
+++ routes/portfolio.route.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const portfolioModel = require("./../models/Portfolio.model");
+const holdingModel = require("./../models/Holding.model");
+const cryptoModel = require("./../models/Crypto.model");
 
-// TEXT
+// Display the portfolio page of each user
+router.get("/:id", (req, res, next) => {
+  portfolioModel
+    .findById(req.params.id)
+    .populate("holdings")
+    .populate("cryptos")
+    .then((portfolio) => {
+      console.log(portfolioModel.findById(req.params.id).populate("holdings"));
+      console.log(portfolio);
+      res.render("portfolio/portfolio-details", { portfolio });
+    })
+    .catch((err) => console.error(err));
+});
 
 module.exports = router;
