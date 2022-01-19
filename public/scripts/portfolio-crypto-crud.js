1087e1a95d438a78ce08d01d7110c1374e9dc441
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-01-19 19:52:21 +0100

    added my pub script

---
+++ public/scripts/portfolio-crypto-crud.js
@@ -0,0 +1,22 @@
+const updateButton = document.querySelector("#update-coin-btn");
+const cancelUpdateButton = document.querySelector("#cancel-update-btn");
+const confirmUpdateButton = document.querySelector("#confirm-update-btn");
+const quantityInput = document.querySelector("#quantity-input");
+
+const sendQuantityInputToBackEnd = () => {
+  const portfolioId = updateButton.getAttribute("data-portfolioid");
+  const holdingId = updateButton.getAttribute("data-holdingid");
+
+  axios
+    .patch(`/dashboard/portfolio/${portfolioId}/holding/${holdingId}/update`)
+    .then((updatedHolding) => {
+      console.log(
+        "This is updatedHolding.data on line 9 of portfolio-crypto-crud script >>>>>",
+        updatedHolding.data
+      );
+      quantityInput.value = updatedHolding.data.quantity;
+    })
+    .catch((err) => console.error(err));
+};
+
+updateButton.addEventListener("click", sendQuantityInputToBackEnd);
