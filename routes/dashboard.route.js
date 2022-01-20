13e0f3c700cff40f9bf38563321c6b6c4d24d70b
Amine <aminehouidi90@gmail.com>
2022-01-20 11:24:55 +0100

    sync for session test

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -4,8 +4,20 @@ const PortfolioModel = require("../models/Portfolio.model");
 const UserModel = require("../models/User.model");
 
 //Dashboard READ
-router.get("/", function (req, res) {
-    const userId = req.session.currentUser._id
+// router.get("/", function (req, res) {
+//     const userId = req.session.currentUser._id
+//     // console.log(userId, "USER ID HERE")
+//     UserModel.findById(userId).populate("portfolios") // PortfolioModel.find({_id: userId})
+//         .then((user) => {
+//             res.render("dashboard/dashboardUser", { user })
+//             // console.log(user, "USER HERE")
+//         })
+//         .catch((err) => console.log(err))
+// });
+
+// A EFFACER  - TEST
+router.get("/:userId", function (req, res) {
+    const userId = req.params.userId
     // console.log(userId, "USER ID HERE")
     UserModel.findById(userId).populate("portfolios") // PortfolioModel.find({_id: userId})
         .then((user) => {
@@ -28,7 +40,7 @@ router.post("/create", function (req, res, next) {
 //Dashboard UPDATE Portfolio
 router.patch("/update/:id", function (req, res, next) {
     PortfolioModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    // console.log(req.body, "EDITED PORTFOLIO HERE");
+        // console.log(req.body, "EDITED PORTFOLIO HERE");
         .then((portfolioEdited) => res.json(portfolioEdited))
         .catch((err) => next(err))
 });
